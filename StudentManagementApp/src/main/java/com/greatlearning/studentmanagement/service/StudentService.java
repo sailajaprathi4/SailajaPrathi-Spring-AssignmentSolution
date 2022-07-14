a4042ff6ee308090855ed3640e56d317eef7fd0e
RasaP <98017006+sailajaprathi4@users.noreply.github.com>
2022-07-13 21:54:31 -0400

    Add files via upload

---
+++ StudentManagementApp/src/main/java/com/greatlearning/studentmanagement/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.greatlearning.studentmanagement.service;
+
+import java.util.List;
+
+import com.greatlearning.studentmanagement.entity.Student;
+
+public interface StudentService {
+
+	List<Student> findAll();
+	
+	void save(Student thestudent);
+	Student findById(int theId);
+	void deleteById(int theId);
+
+
+}
